77169491cf0bda8af121911556c6538c1bbd52ef
unknown <koakpet@yahoo.co.uk>
2025-11-29 09:00:51 +0100

    made a few tweaks to User account popup, to make it more mobile responsive

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -14,7 +14,7 @@ function LogIn() {
   }
 
   return (
-    <div className="m-auto w-10/12 md:w-4/12 h-5/6 flex justify-center items-center">
+    <div className="m-auto w-11/12 md:w-4/12 h-5/6 flex justify-center items-center">
       {isHidden ? (
         <form action="">
           <p className="font-semibold">Sign in</p>
@@ -44,8 +44,8 @@ function LogIn() {
         </form>
       ) : (
         <form action="">
-          <p className="font-semibold">Sign up</p>
-          <div className="mt-3 flex gap-3">
+          <p className="font-semibold mb-3">Sign up</p>
+          <div className="grid grid-cols-2 gap-2 mb-3">
             <input
               type="text"
               id="fName"
@@ -56,35 +56,33 @@ function LogIn() {
               type="text"
               id="lName"
               placeholder="Last Name"
-              className="rounded-md text-md p-1.5  bg-white outline-1 outline-gray-300 focus:outline-green-300"
+              className="rounded-md text-md p-1.5 bg-white outline-1 outline-gray-300 focus:outline-green-300"
             />
           </div>
-
           <input
             type="tel"
             id="phone"
             placeholder="Phone number"
-            className="mt-3 rounded-md text-md p-1.5 w-full bg-white outline-1 outline-gray-300 focus:outline-green-300"
+            className="rounded-md text-md p-1.5 w-full mb-3 bg-white outline-1 outline-gray-300 focus:outline-green-300"
           />
           <input
             type="email"
             id="email"
             placeholder="Email"
-            className="mt-3 rounded-md text-md p-1.5 w-full bg-white outline-1 outline-gray-300 focus:outline-green-300"
+            className="rounded-md text-md p-1.5 w-full mb-3 bg-white outline-1 outline-gray-300 focus:outline-green-300"
           />
           <input
             type="password"
             id="password"
             placeholder="Password"
-            className="mt-3 rounded-md text-md p-1.5 w-full outline-1 bg-white   outline-gray-300 focus:outline-green-300"
+            className="rounded-md text-md p-1.5 w-full mb-3 outline-1 bg-white outline-gray-300 focus:outline-green-300"
           />
-          <button className="mt-3 w-full p-1.5 border border-green-300 bg-green-600 hover:bg-green-700 rounded-md text-white">
+          <button className="w-full p-1.5 border border-green-300 bg-green-600 hover:bg-green-700 rounded-md text-white mb-3">
             Sign up
           </button>
           <p className="text-xs text-center">
-            If you already have an account,
+            If you already have an account,{" "}
             <span className="text-blue-700 cursor-pointer" onClick={signIn}>
-              {" "}
               Sign in
             </span>{" "}
             here
